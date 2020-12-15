1093a1e0cf7f92c758498324e1010dd3d0531a51
yovanproev <yovanproev@yahoo.com>
2020-12-15 17:24:12 +0100

    error on roles fixed

--- src/RoleBasedAccessControl/RoleBaseControl.jsx
+++ src/RoleBasedAccessControl/RoleBaseControl.jsx
@@ -1,16 +1,18 @@
 import Modal from "../Components/Modal/Modal"
 
 export const RenderForAdmin = (props) => {
-  
- if (props.currentUser.Role === "Administrator" || props.currentUser.email === "yovanproev1@yahoo.com") {
+if (props.currentUser.Role === "Administrator") {
  return props.children
  } else return null
 }
 
 export const RenderForOperator = (props) => {
-  if (props.currentUser.Role === "Operator" || props.currentUser.Role === "Administrator") {
+  if (props.currentUser.Role === "Operator") {
     return props.children
     } 
+  else if (props.currentUser.Role === "Administrator") {
+    return props.children
+  }
   else return <Modal show={true}>Please contact the Administrator to get authorization!</Modal>
 }
 
