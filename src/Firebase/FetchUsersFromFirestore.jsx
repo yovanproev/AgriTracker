33d4afa2b4be168a5cfa76ca26e02a18b9810520
yovanproev <yovanproev@yahoo.com>
2020-12-30 18:26:29 +0100

    commit 4

--- src/Firebase/FetchUsersFromFirestore.jsx
+++ src/Firebase/FetchUsersFromFirestore.jsx
@@ -11,30 +11,3 @@ export async function getAllUsers() {
   return arr
 }
   
-// export function getAllUsers() {
-//   return new Promise((resolve) => {
-//   // const snapshot = await users.get();
-//   let arr = []
-//   users.onSnapshot((querySnapshot) => {
-//      querySnapshot.forEach((doc) => {
-//         console.log(doc.data())
-//      return resolve(arr.push(doc.data()))
-      
-//       })
-     
-//   }, (err) => {
-//     console.log(err)
-//      return null
-//   })
-  
-// })
-//   // snapshot.forEach((data) => {
-//   //   // console.log(data)
-//   //   const tableData = data.data()
-//   //   // console.log(data.id, "=>", data.data())
-//   //   arr.push(tableData)
-//   // })
-//   // return arr
-// }
-
-// // getAllUsers1()
